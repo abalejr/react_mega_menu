82647f34ca8f0bc3844eda80340b494ab5b9e1d5
A.J. Bale <abalejr@gmail.com>
2019-04-22 02:44:27 -0500

    Add MegaMenu to App

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import HeroSlider from './components/HeroSlider/HeroSlider';
+import MegaMenu from './components/MegaMenu/MegaMenu';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <MegaMenu />
         <HeroSlider />
       </div>
     );
---
+++ src/components/MegaMenu/MegaMenu.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './MegaMenu.css';
+
+class MegaMenu extends Component {
+  render() {
+    return (
+      <header id="main-navigation">
+        <div className="logo"></div>
+        <div id="PrimaryNav"></div>
+        <div className="account"><a href="#" >Sign In</a></div>
+        <div className="search"><img src={require('../../assets/icon_search.svg')} alt="search" /></div>
+        <div className="cart"><a href="#" >Bag (0)</a></div>
+      </header>
+    );
+  }
+}
+
+export default MegaMenu;
