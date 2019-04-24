d7102b231b4e05b51ac8b8480c5969e2f78cca4e
A.J. Bale <abalejr@gmail.com>
2019-04-24 02:23:38 -0500

    Adjust MegaMenu, finalize styles

--- src/components/MegaMenu/MegaMenu.js
+++ src/components/MegaMenu/MegaMenu.js
@@ -5,11 +5,14 @@ class MegaMenu extends Component {
   render() {
     return (
       <header id="main-navigation">
+        <div className="hamburger-menu"></div>
         <div className="logo"></div>
         <div id="PrimaryNav"></div>
-        <div className="account"><a href="#" >Sign In</a></div>
-        <div className="search"></div>
-        <div className="cart"><a href="#" >Bag (0)</a></div>
+        <div className="header-tools">
+          <div className="account"><a href="#" >Sign In</a></div>
+          <div className="search"></div>
+          <div className="cart"><a href="#" >Bag (0)</a></div>
+        </div>
       </header>
     );
   }
